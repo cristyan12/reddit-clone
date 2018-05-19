f07f47b5aae80fc74aadb90cdbd8f11681b981b2
= <numenor21@gmail.com>
2018-05-19 19:39:56 -0400

    Change some elements of the layout to make it compatible with Bootstrap4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,80 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="{{ app()->getLocale() }}">
 <head>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	
-	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Reddit Clone</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 offset-md-12">
-				<h1>Reddit Clone</h1>
-				<hr>
-				
-				@include('layouts._errors')
+    <div id="app">
+        <nav class="">
+            <div class="container">
+                <div class="">
 
-				@include('layouts._messages')
-				
-				@yield('content')
-			</div>
-		</div>
-	</div> {{-- container --}}
-	
-	{{-- Optional JavaScript --}}
-    {{-- jQuery first, then Popper.js, then Bootstrap JS --}}
-	<script src="{{ asset('js/jquery.js') }}"></script>
-	<script src="{{ asset('js/popper.js') }}"></script>
-	<script src="{{ asset('js/bootstrap.js') }}"></script>
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="">Toggle Navigation</span>
+                        <span class=""></span>
+                        <span class=""></span>
+                        <span class=""></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+
+                <div class="" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="">
+                                <a href="#" class="" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/master', function() {
+	return view('layouts._master');
+});
+
 Route::get('/', 'PostsController@index')->name('posts.index');
 Route::get('posts/create', 'PostsController@create')->name('posts.create');
 Route::post('/', 'PostsController@store')->name('posts.store');
@@ -7,3 +11,7 @@ Route::get('/posts/{post}', 'PostsController@show')->name('posts.show');
 Route::get('/posts/{post}/edit', 'PostsController@edit')->name('posts.edit');
 Route::put('/posts/{post}', 'PostsController@update')->name('posts.update');
 Route::delete('/posts/{post}/delete', 'PostsController@delete')->name('posts.delete');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
