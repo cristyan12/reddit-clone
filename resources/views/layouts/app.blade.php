5476641a59069e1e45588ecaab15ff2544808147
= <numenor21@gmail.com>
2018-05-20 14:35:11 -0400

    Add styles of bootstrap 4 to base template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,88 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="es">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <title>Reddit Clone</title>
 </head>
 <body>
-    <div id="app">
-        <nav class="">
-            <div class="container">
-                <div class="">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            Reddit Clone
+        </a>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="">Toggle Navigation</span>
-                        <span class=""></span>
-                        <span class=""></span>
-                        <span class=""></span>
-                    </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('posts.create') }}">Create Post</a>
+                </li>
+            </ul>
 
-                    <!-- Branding Image -->
-                    <a class="" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+            <ul class="navbar-nav d-flex">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    </li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
 
-                <div class="" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="">
-                                <a href="#" class="" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown" role="menu">
+                            <li class="dropdown-item"><a href="#">Dashboard</a></li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    Out
                                 </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
+                        </ul>
+                    </li>
+                @endguest
+            <ul/>
+            {{-- <form class="form-inline mt-2 mt-md-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form> --}}
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 offset-md-12">
+                <h1>Reddit Clone</h1>
+                <hr>
+                
+                @include('layouts._errors')
+
+                @include('layouts._messages')
+                
+                @yield('content')
             </div>
-        </nav>
-
-        @yield('content')
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+        </div>
+    </div> {{-- container --}}
+    
+    {{-- Optional JavaScript --}}
+    {{-- jQuery first, then Popper.js, then Bootstrap JS --}}
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/popper.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
 </body>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,10 +3,18 @@
 @section('content')
 <div class="row">
 	<div class="col-md-12">
-		<h2>{{ $post->title }}</h2>
-		<p>{{ $post->description}}</p>
-		<p>Updated {{ $post->updated_at->diffForHumans() }}</p>
-		<p>Created {{ $post->created_at->diffForHumans() }}</p>
+		<div class="card">
+			<div class="card-header">
+				<h2>{{ $post->title }}</h2>
+			</div>
+			<div class="card-body">
+				<p>{{ $post->description}}</p>
+				<p>
+					<a href="{{ url($post->url) }}">{{ $post->url }}</a>
+				</p>
+				<p>Created {{ $post->created_at->diffForHumans() }}</p>
+			</div>
+		</div>
 	</div>
 </div>
 <hr>
